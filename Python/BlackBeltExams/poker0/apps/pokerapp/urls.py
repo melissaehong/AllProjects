36bd8c9bbbb3f6edbfa71de7e6ad1243fc14b633
Melissa Hong <melissahong1@gmail.com>
2017-09-15 21:16:58 -0700

    day 54

--- Python/BlackBeltExams/poker0/apps/pokerapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import url
-from . import views
-
-urlpatterns = [
-    url(r'^$', views.index, name = 'index'),
-    url(r'^(?P<poker_id>\d+)*/(?P<pokee_id>\d+)/poke$', views.poke, name = 'poke'),
-]
