37549041c4452556e27ef6cd389b595b4b1db581
Melissa Hong <melissahong1@gmail.com>
2017-10-06 22:20:55 -0700

    swift intro

--- iOS/stringconcat.playground/Contents.swift
+++ iOS/stringconcat.playground/Contents.swift
@@ -6,3 +6,16 @@ var str = "Hello, playground"
 str += " you are so cool"
 print(str)
 
+var name = "anakin"
+
+print ("The maximum value \(Int.max)")
+
+print ("The minimum value \(Int.min)")
+
+print ("The maximum value \(Int32.max)")
+
+print ("The minimum value \(Int32.min)")
+
+print("The maximum value \(UInt32.max)")
+
+print("The minimum value \(UInt32.min)")
